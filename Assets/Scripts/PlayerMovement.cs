943cd1ed23deae66a61d2f8a986ebb24fc5bae93
Orim2002 <92533958+Orim2002@users.noreply.github.com>
2025-02-23 05:59:55 +0200

    Add Mario bounce

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -86,6 +86,12 @@ public class PlayerMovement : MonoBehaviour
 
     public void OollisionEnter2D(Collision2D collision)
     {
+        if (collision.gameObject.layer != LayerMask.NameToLayer("Enemy")) {
+            if (transform.DotTest(collision.transform, Vector2.down)) {
+                velocity.y = jumpForce / 2f;
+                jumping = true;
+            }
+        }
         if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUP")) {
             if (transform.DotTest(collision.transform, Vector2.up)) {
                 velocity.y = 0f;
